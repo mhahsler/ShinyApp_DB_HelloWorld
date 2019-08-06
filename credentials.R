bd837f245b7cbc7692ce12028b6ee0a8a2d78d71
Michael Hahsler <michael@hahsler.net>
2019-08-06 10:54:04 -0500

    Update.

--- credentials.R
+++ credentials.R
@@ -2,7 +2,7 @@
 
 options(database_server = "classroomdb.smu.edu")
 options(database_port = 55433)
-options(database_name = "ShinyTest")
-options(database_userid = "shinyapp")
-options(database_password = "N0tSecured43887")
+options(database_name = "your_username")
+options(database_userid = "your_userid")
+options(database_password = "your_password")
 
